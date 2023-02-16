1a826b62766fe60e21be18c3c786620fbc64a521
Behnam <Behnamnazari80@gmail.com>
2023-02-15 16:01:24 -0800

    SM.Domain, ShopManagement.Application.contracts : initialized ShopManagement.application: working on

---
+++ LampShade/0_Framework/Application/OperationResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Framework.Application
+{
+    public class OperationResult
+    {
+        public bool IsSucceeded { get; set; }
+        public string Message { get; set; }
+
+        public OperationResult()
+        {
+            IsSucceeded= false;
+        }
+        public OperationResult Succeeded(string message = "Success") 
+        {
+            Message= message;
+            IsSucceeded=true;
+            return this;
+        
+        }
+
+        public OperationResult Failed(string message)
+        {
+            Message=message;
+            IsSucceeded=false; 
+            return this;
+        }
+    }
+}
---
+++ LampShade/ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,41 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication: IProductCategoryApplication
+
+    {
+        private readonly IProductCategoryRepository _productCategoryRespository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRespository)
+        {
+            _productCategoryRespository = productCategoryRespository;
+        }
+
+        public void Create(CreateProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public List<ProductCategoryViewModel> GetProductCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
